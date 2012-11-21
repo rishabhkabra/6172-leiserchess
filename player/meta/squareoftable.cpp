8a248ce06e4e40f0b0dd5b849230c2a9e98b2cac
Rishabh Kabra <rkabra@albuterol.csail.mit.edu>
2012-11-21 00:20:36 -0500

    Corrected meta-programmed tables for rank and file to correctly represent sentinals and board values.

--- player/meta/squareoftable.cpp
+++ player/meta/squareoftable.cpp
@@ -2,17 +2,29 @@
 #include <cstdio>
 
 int main() {
-  std::cout<<"unsigned char fil_of_table[144] = {\n";
+  std::cout<<"unsigned char fil_of_table[144] = {\n\t";
   for (int i = 0; i < 144; i++) {
-    std::cout<<(i/12)<<", ";
-    if (i % 12 == 11) std::cout<<"\n";
+    int f;
+    if (i < 12 || i >= 132 || (i % 12 == 0) || (i % 12 == 11)) {
+      f = 10;
+    } else {
+      f = i/12 - 1;
+    }
+    printf("'\\x%x', ", f);
+    if (i % 12 == 11) std::cout<<"\n\t";
   }
   std::cout<<"};\n";
 
-  std::cout<<"\nunsigned char rnk_of_table[144] = {\n";
+  std::cout<<"\nunsigned char rnk_of_table[144] = {\n\t";
   for (int i = 0; i < 144; i++) {
-    std::cout<<(i%12)<<", ";
-    if (i % 12 == 11) std::cout<<"\n";
+    int r;
+    if (i < 12 || i >= 132 || (i % 12 == 0) || (i % 12 == 11)) {
+      r = 10;
+    } else {
+      r = i%12 - 1;
+    }
+    printf("'\\x%x', ", r);
+    if (i % 12 == 11) std::cout<<"\n\t";
   }
   std::cout<<"};\n";
 
